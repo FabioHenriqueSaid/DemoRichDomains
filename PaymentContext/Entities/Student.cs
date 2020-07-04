b13bf8d6486a03d6bf9076dc2636b957e0d0fbf3
fabio said <fabiosaid635@gmail.com>
2020-07-03 21:01:07 -0300

    Entity: Student

---
+++ PaymentContext/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContextDomain.Entities
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+    }
+}
