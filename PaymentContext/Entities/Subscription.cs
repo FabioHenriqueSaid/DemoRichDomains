542d5594459d124212ae6b36a2671a6edf191264
fabio said <fabiosaid635@gmail.com>
2020-07-03 21:04:52 -0300

    Entity: Subscription

---
+++ PaymentContext/Entities/Subscription.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContextDomain.Entities
+{
+    public class Subscription
+    {
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public DateTime? ExpireDate { get; set; }
+    }
+}
