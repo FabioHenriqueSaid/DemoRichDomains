63d925367c9994b914f29a7814606234dfed6508
fabio said <fabiosaid635@gmail.com>
2020-08-10 15:27:14 -0300

    Entity: Client

--- ExchangeContextDomain/Entities/Client.cs
+++ ExchangeContextDomain/Entities/Client.cs
@@ -3,9 +3,19 @@ namespace ExchangeContextDomain.Entities
 {
     public class Client
     {
+        public Client(string firstName, string lastName, string document, string email, DateTime birthDate)
+        {
+            CreationDate = DateTime.Now;
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
         public DateTime CreationDate { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
         public string Document { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
---
+++ PaymentContextTests/Entities/ClientTests.cs
@@ -0,0 +1,20 @@
+﻿using ExchangeContextDomain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PaymentContextTests.Entities
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var client = new Client("Fábio",
+                "Henrique",
+                "13227956544",
+                "fabiosaid635@gmail.com",
+               DateTime.Now);
+        }
+    }
+}
