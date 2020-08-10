9648424aef1451563aa87dacbdba54d653ae6c85
fabio said <fabiosaid635@gmail.com>
2020-08-10 17:10:59 -0300

    Entity: Order

--- ExchangeContextDomain/Entities/Order.cs
+++ ExchangeContextDomain/Entities/Order.cs
@@ -10,13 +10,21 @@ namespace ExchangeContextDomain.Entities
             Active = true;
         }
         public DateTime CreateDate { get; private set; }
+        public DateTime EndDate { get; private set; }
         public bool Active { get; private set; }
         public string BitcoinQuote { get; private set; }
 
+        public void CancelOrder() {
+            EndDate = DateTime.Now;
+            Active = false;
+        }
+
+
         /*
            Cancelar Ordem (usuario),
-           Cancelar Ordem (Porque foi executada)
-           Tipo de ordem (Compra/Venda)
+           Cancelar Ordem (Porque foi executada),
+           Tipo de ordem (Compra/Venda),
+           Rate
          */
     }
 }
