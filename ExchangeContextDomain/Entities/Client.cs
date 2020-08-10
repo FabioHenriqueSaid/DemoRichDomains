be7780f2a88031bbe9af6be89af6c77e5d9cb294
fabio said <fabiosaid635@gmail.com>
2020-08-10 09:47:41 -0300

    Entity: Client

---
+++ ExchangeContextDomain/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace ExchangeContextDomain.Entities
+{
+    public class Client
+    {
+        /*
+         - Campos
+            Nome,
+            CPF,
+            Endereço,
+            Data Nascimento,
+            Carteira,
+            Email
+        */
+    }
+}
