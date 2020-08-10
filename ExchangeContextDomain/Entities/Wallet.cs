b6238eac5059ba7eb15d720184181aa1492ecbcf
fabio said <fabiosaid635@gmail.com>
2020-08-10 16:54:00 -0300

    Entity: Wallet, Order

--- ExchangeContextDomain/Entities/Wallet.cs
+++ ExchangeContextDomain/Entities/Wallet.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ExchangeContextDomain.Entities
 {
     public class Wallet
     {
+        public IList<Order> _orders;
         public string CodeWallet { get; private set; }
         public string BitcoinBalance { get; private set; }
         public string Reaisbalance { get; private set; }
-        public List<Order> Orders { get; private set; }
+
+        //Retorna todas as ordens
+        public  IReadOnlyCollection<Order> Orders { get { return _orders.ToArray();  } }
+
         public void BuyBitcoin(string valor) {
             //Verificar se possui saldo
             //Quando compra adiciona uma ordem de compra    
