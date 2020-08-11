3f4c9316f5b6b2c138d5a5073e42c7df24466aef
fabio said <fabiosaid635@gmail.com>
2020-08-11 15:26:44 -0300

    Entity : abstract class Entity

--- ExchangeContextDomain/ValueObjects/Enums/EDocumentType.cs
+++ ExchangeContextDomain/ValueObjects/Enums/EDocumentType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ExchangeContextDomain.ValueObjects.Enums
+﻿namespace ExchangeContextDomain.ValueObjects.Enums
 {
     public enum EDocumentType
     {
---
+++ ExchangeShared/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ExchangeContextShared
+{
+    public abstract class Entity
+    {
+        public Entity() {
+            Id = Id;
+        }
+
+        public Guid Id { get; set; }
+    }
+}
