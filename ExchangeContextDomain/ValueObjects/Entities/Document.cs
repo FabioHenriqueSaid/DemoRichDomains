b701fcd400f7ec3ddf36a477fef97031caf1a289
fabio said <fabiosaid635@gmail.com>
2020-08-12 16:41:52 -0300

    Document

--- ExchangeContextDomain/ValueObjects/Entities/Document.cs
+++ ExchangeContextDomain/ValueObjects/Entities/Document.cs
@@ -1,15 +1,37 @@
-﻿using ExchangeContextDomain.ValueObjects.Enums;
-using ExchangeContextShared.ValueObjects;
+﻿
 
+using ExchangeContextDomain.ValueObjects.Enums;
+using System.Diagnostics.Contracts;
+using ExchangeContextShared.ValueObjects;
+using Flunt.Validations;
 namespace ExchangeContextDomain.ValueObjects.Entities
 {
     public class Document : ValueObject
     {
-        public Document(string number)
+        public Document(string number, EDocumentType type)
         {
             Number = number;
+            Type = type;
+
+           AddNotifications(new Contract()
+                .Requires
+                .IsTrue(Validate(), "Document.Number","Documento inválido")
+                );
+
         }
         public string Number { get; private set; }
-        public EDocumentType MyProperty { get; private set; }
+        public EDocumentType Type { get; private set; }
+
+        public bool Validate()
+        {
+            //Validação Temporaria
+            if (Type == EDocumentType.CNPJ && Number.Length == 14)
+                return true;
+
+            if (Type == EDocumentType.CPF && Number.Length == 11)
+                return true;
+
+            return false;
+        }
     }
 }
