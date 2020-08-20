72b4a59e65e2b9350ce97693985e5fd33e551cee
fabio said <fabiosaid635@gmail.com>
2020-08-20 15:23:31 -0300

    Add Handles

---
+++ ExchangeShared/Commands/ICommandResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeContextShared.Commands
+{
+    public interface ICommandResult
+    {
+    }
+}
---
+++ ExchangeShared/Handles/IHandle.cs
@@ -0,0 +1,9 @@
+﻿using ExchangeContextShared.Commands;
+
+namespace ExchangeContextShared.Handles
+{
+    public interface IHandle<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
